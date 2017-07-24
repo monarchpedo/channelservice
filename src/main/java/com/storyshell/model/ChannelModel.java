458df8f74ae6b4ed6d824c416346b14cf96d8f75
monarchpedo <rbosemonarch@gmail.com>
2017-07-24 18:04:08 +0530

    added model classes to channelservices to master

---
+++ src/main/java/com/storyshell/model/ChannelModel.java
@@ -0,0 +1,88 @@
+package com.storyshell.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class ChannelModel implements Serializable {
+	private int channelId;
+	private String channelName;
+	private String pageTitle;
+	private String pageDescription;
+	private String pageId;
+	private String createdDate;
+	private String modifiedDate;
+	private List<Post> post;
+	private List<MediaModel> mediaList;
+
+	public int getChannelId() {
+		return channelId;
+	}
+
+	public void setChannelId(int channelId) {
+		this.channelId = channelId;
+	}
+
+	public String getChannelName() {
+		return channelName;
+	}
+
+	public void setChannelName(String channelName) {
+		this.channelName = channelName;
+	}
+
+	public String getPageTitle() {
+		return pageTitle;
+	}
+
+	public void setPageTitle(String pageTitle) {
+		this.pageTitle = pageTitle;
+	}
+
+	public String getPageDescription() {
+		return pageDescription;
+	}
+
+	public void setPageDescription(String pageDescription) {
+		this.pageDescription = pageDescription;
+	}
+
+	public String getPageId() {
+		return pageId;
+	}
+
+	public void setPageId(String pageId) {
+		this.pageId = pageId;
+	}
+
+	public String getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(String createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(String modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public List<Post> getPost() {
+		return post;
+	}
+
+	public void setPost(List<Post> post) {
+		this.post = post;
+	}
+
+	public List<MediaModel> getMediaList() {
+		return mediaList;
+	}
+
+	public void setMediaList(List<MediaModel> mediaList) {
+		this.mediaList = mediaList;
+	}
+}
